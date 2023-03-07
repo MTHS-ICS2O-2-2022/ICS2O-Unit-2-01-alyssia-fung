b4c4d266b65ce6eaab3368fdece03acdb907f59f
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-03-07 18:08:44 +0000

    My second clickable webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Alyssiafung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssia Fung
+// Created on: march 2023
 // This file contains the JS functions for index.html
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "Hello, World!"
+}
