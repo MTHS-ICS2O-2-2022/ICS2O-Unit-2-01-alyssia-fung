74c2b99ca2f312b341fcf3b4b0ce817e1b241d79
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-03-08 17:59:41 +0000

    fixed

--- go_app/main.go
+++ go_app/main.go
@@ -8,7 +8,7 @@ package main
 import "fmt"
 
 func main() {
-    // This function displays, "Hello, World!"
+    // This function displays,"Hello, World!"
     // the Go standard is to indent using "tabs" NOT spaces!
     fmt.Println("Hello, World!")
     
